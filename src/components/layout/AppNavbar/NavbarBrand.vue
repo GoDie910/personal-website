b20e6c107fd07bba26778da168e7d64af9c34255
GoDie910 <diego_shot100@hotmail.com>
2026-07-01 11:57:33 -0500

    Update NavbarBrand.vue

--- src/components/layout/AppNavbar/NavbarBrand.vue
+++ src/components/layout/AppNavbar/NavbarBrand.vue
@@ -3,34 +3,36 @@
     to="/"
     class="logo"
   >
-    DBA
+    <img
+      :src="logo"
+      alt="Logo"
+    >
   </RouterLink>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+  import { RouterLink } from 'vue-router'
+  import logo from '@/assets/logo.svg'
 </script>
 
 <style scoped>
-.logo {
-  font-size: clamp(1.2rem, 1.25vw, 6.6vh);
-
-  font-weight: 800;
-
-  letter-spacing: 0.2em;
-
-  color: white;
-
-  text-decoration: none;
-
-  transition:
-    opacity 0.2s ease,
-    transform 0.2s ease;
-}
-
-.logo:hover {
-  opacity: 0.7;
-
-  transform: translateY(-0.1rem);
-}
+  .logo {
+    display: inline-flex;
+    align-items: center;
+    text-decoration: none;
+    transition:
+      opacity 0.2s ease,
+      transform 0.2s ease;
+  }
+  
+  .logo img {
+    display: block;
+    width: 48px;   /* or whatever size you want */
+    height: auto;
+  }
+  
+  .logo:hover {
+    opacity: 0.7;
+    transform: translateY(-0.1rem);
+  }
 </style>
